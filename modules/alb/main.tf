e624f84375d079a44899781441d8e6dca7ea9c73
Enyioma Nwadibia <enyiomanwadibia@gmail.com>
2026-01-29 20:02:40 +0000

    Test: Add monitoring configuration

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -74,18 +74,27 @@ resource "aws_lb_listener" "http" {
   tags = var.tags
 }
 
-# HTTPS Listener (optional, requires certificate)
-resource "aws_lb_listener" "https" {
-  count             = var.enable_https ? 1 : 0
+# HTTP Listener - Security compliant
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = var.certificate_arn
+  port              = "80"
+  protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
+    type = var.enable_https ? "redirect" : "forward"
+    
+    # Redirect to HTTPS (AWS Security Hub ELB.1 compliance)
+    dynamic "redirect" {
+      for_each = var.enable_https ? [1] : []
+      content {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    
+    # Forward for dev/testing only
+    target_group_arn = var.enable_https ? null : aws_lb_target_group.main.arn
   }
 
   tags = var.tags
