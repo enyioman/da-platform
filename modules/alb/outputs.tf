3c95e52005064bc1fee433423cb2727830c2d947
Enyioma Nwadibia <enyiomanwadibia@gmail.com>
2026-01-29 21:28:24 +0000

    Test: Add monitoring configuration

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -18,10 +18,10 @@ output "target_group_arn" {
   value       = aws_lb_target_group.main.arn
 }
 
-output "http_listener_arn" {
-  description = "ARN of the HTTP listener"
-  value       = aws_lb_listener.http.arn
-}
+# output "http_listener_arn" {
+#   description = "ARN of the HTTP listener"
+#   value       = aws_lb_listener.http.arn
+# }
 
 output "https_listener_arn" {
   description = "ARN of the HTTPS listener (if enabled)"
